1332e0bd8a468974bcb8e8c3b7dfae3c9cd5b50a
Gyumin Bae <bgmsound0528@gmail.com>
2025-01-23 18:52:04 +0900

    Setup Log4j2

--- baegyum-in-infrastructure/gateway/build.gradle.kts
+++ baegyum-in-infrastructure/gateway/build.gradle.kts
@@ -2,8 +2,18 @@ plugins {
     alias(libs.plugins.spring.boot)
 }
 
+configurations {
+    configureEach {
+        exclude(
+            group = libs.spring.boot.starter.logging.get().group,
+            module = libs.spring.boot.starter.logging.get().module.name
+        )
+    }
+}
+
 dependencies {
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.log4j2)
     rootProject.subprojects.filter { project != it.project }.forEach {
         runtimeOnly(project(it.path))
     }
